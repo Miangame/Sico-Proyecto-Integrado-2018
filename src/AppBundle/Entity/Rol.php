e45b9c2f7fb3a59c7c281a2f0cdf69e076ae321b
Miguel Ángel Gavilán Merino <miangame1@gmail.com>
2018-05-03 22:46:49 +0200

    Configuring fos user

---
+++ src/AppBundle/Entity/Rol.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="rol")
+ */
+class Rol
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $rol;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set rol
+     *
+     * @param string $rol
+     *
+     * @return Rol
+     */
+    public function setRol($rol)
+    {
+        $this->rol = $rol;
+
+        return $this;
+    }
+
+    /**
+     * Get rol
+     *
+     * @return string
+     */
+    public function getRol()
+    {
+        return $this->rol;
+    }
+}
