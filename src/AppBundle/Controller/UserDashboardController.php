c7efc512829ac5508ed78d0b6c42df666ec69d81
Javier Ponferrada López <jponferrada26@gmail.com>
2018-05-08 19:19:11 +0200

    new controller User_Dashboard

---
+++ src/AppBundle/Controller/UserDashboardController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * Class UserDashboardController
+ * @package AppBundle\Controller
+ *
+ * @Route("/user", name="user_dashboard")
+ */
+class UserDashboardController extends Controller
+{
+
+}
