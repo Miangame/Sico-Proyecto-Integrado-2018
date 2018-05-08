0dc73e4ae7b8d6102b85eb77879c8b0372fccf1c
Miguel Ángel Gavilán Merino <miangame1@gmail.com>
2018-05-08 18:43:15 +0200

    Changed init page to login

---
+++ src/AppBundle/Controller/CommonController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+
+/**
+ * @Route("/", name="dashboard")
+ * @Security("has_role('ROLE_USER')")
+ */
+class CommonController extends Controller
+{
+    /**
+     * @Route("/", name="dashboard")
+     */
+    public function dashboardAction(Request $request)
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        if ($user->hasRole("ROLE_ADMIN")) {
+            return $this->redirect($this->generateUrl('panel_dashboard'));
+        } else if ($user->hasRole("ROLE_USER")) {
+            return $this->redirect($this->generateUrl('user_dashboard'));
+        }
+
+        die("Error");
+
+    }
+}
