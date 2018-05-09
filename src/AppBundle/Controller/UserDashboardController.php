3e488cf0aca3362e53dbc0afe37674ea015c45cd
Javier Ponferrada López <jponferrada26@gmail.com>
2018-05-09 18:58:08 +0200

    add dashboard menuUser

--- src/AppBundle/Controller/UserDashboardController.php
+++ src/AppBundle/Controller/UserDashboardController.php
@@ -20,14 +20,10 @@ class UserDashboardController extends Controller
      */
     public function dashboardAction(Request $request)
     {
-        /** @var User $user */
         $user = $this->getUser();
-        dump($user->getRol());
-        die();
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('user/dashboard/view.html.twig', array(
+            'user' => $user
+        ));
     }
 }
 
