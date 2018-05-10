7c35a9d642701b8651d7c4837554d431f4ff099a
Javier Ponferrada López <jponferrada26@gmail.com>
2018-05-10 19:36:30 +0200

    First commit user_fct

---
+++ src/AppBundle/Controller/UserFctController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\User;
+use AppBundle\Repository\CompanyRepository;
+use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class UserDashboardController
+ * @package AppBundle\Controller
+ *
+ * @Route("/user")
+ */
+class UserFctController extends Controller
+{
+    /**
+     * @Route("/user/fct", name="user_fct")
+     */
+    public function dashboardAction(Request $request)
+    {
+        /** @var EntityManager $em */
+        $em = $this->get("doctrine.orm.entity_manager");
+        /** @var CompanyRepository $companyRepository */
+        $companyRepository = $em->getRepository("AppBundle:Company");
+
+        return $this->render('user/fct/view.html.twig', array(
+            'companies' => $companyRepository->getCompanies()
+        ));
+    }
+}
