fa712e62f8692d05758bd45f96c53711119f37b4
Miguel Ángel Gavilán Merino <miangame1@gmail.com>
2018-06-06 22:22:55 +0200

    New admin configuration

--- src/AppBundle/Controller/PanelConfigController.php
+++ src/AppBundle/Controller/PanelConfigController.php
@@ -3,6 +3,8 @@
 namespace AppBundle\Controller;
 
 
+use AppBundle\Form\ConfigAdminType;
+use AppBundle\Services\ConfigGeneralHelper;
 use AppBundle\Services\UsersHelper;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,6 +17,40 @@ class PanelConfigController extends Controller
      */
     public function index(Request $request)
     {
-        return $this->render('panel/config/view.html.twig', array());
+        /** @var ConfigGeneralHelper $current_config */
+        $current_config = $this->get('app.configHelper')->getConfig();
+
+        $form_config_global = $this->formConfiguration(
+            ConfigAdminType::class, $current_config, array(), $request);
+
+        if ($form_config_global == "ok")
+            return $this->redirectToRoute('panel_dashboard');
+
+        return $this->render('panel/config/view.html.twig', array(
+            'formConfig' => $form_config_global->createView()
+        ));
+    }
+
+    public function formConfiguration($classType, $class_instance, $options, $request)
+    {
+        $form = $this->createForm($classType, $class_instance, $options);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $formRequest = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($formRequest);
+            $entityManager->flush();
+
+            $request->getSession()
+                ->getFlashBag()
+                ->add('success', 'Configuración aplicada');
+
+            return "ok";
+
+        }
+
+        return $form;
     }
 }
