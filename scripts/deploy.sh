91575bf07ec984048c4f97c1b88da9a1d437031a
Miguel Ángel Gavilán Merino <miangame1@gmail.com>
2018-06-18 12:52:24 +0200

    Update script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,6 @@ php bin/console cache:clear --env=prod
 php bin/console doctrine:schema:update -f
 php bin/console cache:warmup --env=prod
 php bin/console asset:install
-npm install
 chmod 777 var/cache/* -R
 chmod 777 var/logs/* -R
 chmod 777 var/sessions/* -R
